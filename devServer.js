cfaec9232238dc1d50104a5d779b38cbe0122253
eugene <eugeneyaroshenko@gmail.com>
2017-10-21 02:39:28 +0300

    changed route

--- devServer.js
+++ devServer.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
