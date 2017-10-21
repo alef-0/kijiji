3e05e8d79c5fc9aeff740daf2b6870dd8242100c
eugene <eugeneyaroshenko@gmail.com>
2017-10-21 03:19:24 +0300

    changed server configs-3

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -9,8 +9,7 @@ module.exports = {
     ],
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/'
+        filename: 'bundle.js'
     },
     plugins: [
         new webpack.optimize.OccurenceOrderPlugin(),
