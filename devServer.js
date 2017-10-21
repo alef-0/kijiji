8d053cd35d24c0c83270f7fdc50076698217d2ec
eugene <eugeneyaroshenko@gmail.com>
2017-10-21 03:20:20 +0300

    changed server configs-4

--- devServer.js
+++ devServer.js
@@ -10,7 +10,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static('dist'));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/dist/index.html'));
