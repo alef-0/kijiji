9879d3ec95f4987a7d694e5a27b6e0d3a34a98ba
eugene <eugeneyaroshenko@gmail.com>
2017-10-21 03:23:08 +0300

    changed server configs-45

--- devServer.js
+++ devServer.js
@@ -13,7 +13,8 @@ const port = process.env.PORT || 3000;
 app.use(express.static('dist'));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/dist/index.html'));
+    console.warn(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.use('/verify', function(req, res, next) {
