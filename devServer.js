a7f579c5036c55d7ca5149778d78cd7891512b72
eugene <eugeneyaroshenko@gmail.com>
2017-11-08 23:02:03 +0200

    Added static express

--- devServer.js
+++ devServer.js
@@ -13,6 +13,8 @@ const io = socket_io.listen(server);
 
 const port = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
